849e2e3f7276c31377463fbbbe5753dfd9316a0c
Łukasz Mazurek <lukasz.mazurek@netguru.co>
2017-12-08 13:09:55 +0100

    initial form component architecture

---
+++ components/FormSection.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="section">
+    <div v-for="item in items">
+      <form-element :element="item" />
+    </div>
+  </div>
+</template>
+
+<script>
+import FormElement from '~/components/FormElement'
+export default {
+  components: {
+    FormElement
+  },
+  props: {
+    items: Array
+  }
+}
+</script>
+
+<style>
+
+.section {
+  border: 2px solid black
+}
+
+</style>
